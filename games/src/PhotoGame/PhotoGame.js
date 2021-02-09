efdfe5814cef8a668c7123893021f56273fee6ab
Michal <karnath.michal@gmail.com>
2021-02-09 13:44:14 +0100

    button next added

--- games/src/PhotoGame/PhotoGame.js
+++ games/src/PhotoGame/PhotoGame.js
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Images from "./Images";
 import { photos } from "./photos";
 import "./photoGame.css";
 
 const PhotoGame = () => {
-  const random = photos[Math.floor(Math.random() * photos.length)];
+  // const random = photos[Math.floor(Math.random() * photos.length)];
   const [value, setValue] = useState("");
-  const [animal, setAnimal] = useState(random.description);
-  const [photoImg, setPhotoImg] = useState(random.photo);
+  const [index, setIndex] = useState(0);
+  const [animal, setAnimal] = useState(photos[index].description);
+  const [photoImg, setPhotoImg] = useState(photos[index].photo);
   const [win, setWin] = useState(false);
 
   const handleOnChange = (e) => {
@@ -15,21 +16,30 @@ const PhotoGame = () => {
     setValue(e.target.value);
   };
 
+  useEffect(() => {
+    setAnimal(photos[index].description);
+    setPhotoImg(photos[index].photo);
+  }, [index]);
+
   const handleOnSubmit = (e) => {
     e.preventDefault();
     if (value === animal) {
       console.log("brawo");
       setWin(true);
-    } else {
-      setValue("");
     }
+    setValue("");
+  };
+
+  const handleOnNext = () => {
+    setIndex(index + 1);
+    setWin(false);
   };
 
   return (
     <div className="photoGameContainer">
       <Images win={win} images={photoImg} />
       {win ? (
-        <h2>Brawo!!! wygrałeś, hasło to => {animal}</h2>
+        <h2>Brawo!!! wygrałeś, hasło to {animal}</h2>
       ) : (
         <div className="inputContainer">
           <form onSubmit={handleOnSubmit}>
@@ -44,6 +54,12 @@ const PhotoGame = () => {
           </form>
         </div>
       )}
+      {index === photos.length - 1 && win ? (
+        <button>Sprawdź wynik</button>
+      ) : null}
+      {index !== photos.length - 1 ? (
+        <button onClick={handleOnNext}>Next</button>
+      ) : null}
     </div>
   );
 };
